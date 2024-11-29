001b4aeec25da03f81a07ab2c9ba4cf389d68c7a
Nimira43 <n.rabone72@aol.com>
2024-11-29 20:08:35 +0000

    Showing the results of the users request. Improving the look of the output.

--- Fibonacci -Calculator/main.py
+++ Fibonacci -Calculator/main.py
@@ -1,10 +1,17 @@
-print('The Fibonacci Calculator')
+print('--------------------------')
+print('|The Fibonacci Calculator|')
+print('--------------------------')
 
 num = int(input('How many digits of the Fibonacci Sequence do you want to compute: '))
+print   ('--------------------------------------------------------------------')
 
 fib = [1, 1]
 for i in range(num-2):
   new_fib = fib[i] + fib[i+1]
   fib.append(new_fib)
-print(fib)
+
+print("The first " + str(num) + " numbers of the Fibonacci Sequence are: ")
+for number in fib:
+  print(number)
+print   ('--------------------------------------------------')
 
