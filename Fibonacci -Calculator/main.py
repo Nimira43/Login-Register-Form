67f3dd9a6de419f176a52e54843f0fc2d5d1033d
Nimira43 <n.rabone72@aol.com>
2024-11-29 20:17:43 +0000

    Then calculating the Golden Ratios

--- Fibonacci -Calculator/main.py
+++ Fibonacci -Calculator/main.py
@@ -3,15 +3,21 @@ print('|The Fibonacci Calculator|')
 print('--------------------------')
 
 num = int(input('How many digits of the Fibonacci Sequence do you want to compute: '))
-print   ('--------------------------------------------------------------------')
 
 fib = [1, 1]
 for i in range(num-2):
   new_fib = fib[i] + fib[i+1]
   fib.append(new_fib)
 
-print("The first " + str(num) + " numbers of the Fibonacci Sequence are: ")
+print("\nThe first " + str(num) + " numbers of the Fibonacci Sequence are: ")
 for number in fib:
   print(number)
-print   ('--------------------------------------------------')
 
+golden = []
+for i in range(len(fib)-1):
+  ratio = fib[i+1]/fib[i]
+  golden.append(ratio)
+
+print("\nThe corresponding Golden Ratio values are: ")
+for ration in golden:
+  print(ratio)
